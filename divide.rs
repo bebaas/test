c2645339c0d3241b26cb3acaabece5537f9440d1
bebaas <wusheng815@126.com>
2023-11-10 10:49:33 +0800

    Create divide.rs
    create a file for test.

---
+++ divide.rs
@@ -0,0 +1,23 @@
+fn u64_divide(dividend: u64, divisor: u64) -> u64 {
+    let mut quotient = 0;
+    let mut remainder = 0;
+
+    for i in (0..64).rev() {
+        remainder = (remainder << 1) | ((dividend >> i) & 1);
+
+        if remainder >= divisor {
+            remainder -= divisor;
+            quotient |= 1 << i;
+        }
+    }
+
+    quotient
+}
+
+fn main() {
+    let dividend = 1234567890;
+    let divisor = 987654321;
+
+    let quotient = u64_divide(dividend, divisor);
+    println!("Quotient: {}", quotient);
+}
